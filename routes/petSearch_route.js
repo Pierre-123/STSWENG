dc2e8f7123da846210e54b002702ea8e49c8844d
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-03-18 11:40:19 +0800

    PetSearch 1.1
    Nothing much changed other than attempting to POST petSearch, doesn't work, need help here. Getting closer but also slowing down here as new concern.
    
    Do we set a page limit? Does it keep scrolling down? Amount of results/pets per page limit?
    
    Also updated messages to properly reroute back to petSearch

--- routes/petSearch_route.js
+++ routes/petSearch_route.js
@@ -12,5 +12,29 @@ router.get('/', async (req,res)=>{
     }
 });
 
+router.post('/search', async (req, res) => {
+    const { species, breed, age, size } = req.body;
+    console.log("Data to be finding: ")
+    console.log(req.body)
+    try {
+        let query = {};
+        if (species) query.species = species;
+        if (breed) query.breed = breed;
+        if (age) query.age = age;
+        if (size) query.size = size;
+
+        const pets = await Pet.find(query).lean();
+        console.log("Data found: ")
+        console.log(pets) //doesn't find any? even with right stuff hmm
+        /*res.render('petSearch', { 
+            pets: pets, 
+            title: "Pet Search",
+            submittedValues: req.body  // Pass submitted form values back to template
+        });*/
+    } catch (err) {
+        console.error("Error searching pets:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = router;
